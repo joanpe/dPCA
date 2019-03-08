d9109ebab09b6f3c956dcce21ef9d63ef1012a04
joanpe <joanperramonllussa@gmail.com>
2019-03-08 14:16:48 +0100

    Some changes and add plots for plotting 1rst components.

--- python/dPCA/Example/multivar.py
+++ python/dPCA/Example/multivar.py
@@ -19,6 +19,7 @@ from dPCA import dPCA
 N, T, S = 3, 10, 2
 
 n_trials = 100  
+noise = 0.2
 
 # Definition of the correlation matrix between the 3 neurons and means
 a = np.random.randint(5, size=(N, N))  # random matrix
@@ -26,7 +27,7 @@ np.fill_diagonal(a, 1)
 corr = np.tril(a) + np.tril(a, -1).T
 
 mean1 = [0, 0, 0]
-mean2 = [5, 3, 2]
+mean2 = [20, 20, 20]
 means = [mean1, mean2]
 
 # Getting the trial by trial data for each time point
@@ -35,8 +36,8 @@ Xtrial = np.zeros((n_trials, N, T, S))
 for t in range(T):
     x1 = np.random.multivariate_normal(means[0], corr, size=n_trials)
     x2 = np.random.multivariate_normal(means[1], corr, size=n_trials)
-    Xtrial[:, :, t, 0] = x1
-    Xtrial[:, :, t, 1] = x2
+    Xtrial[:, :, t, 0] = x1*noise+2*t
+    Xtrial[:, :, t, 1] = x2*noise+2*t
 
 
 # trial-average data
@@ -76,22 +77,22 @@ X_proj = dpca.fit_transform(Xc, Xtrial)
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 for t in range(T):
-    ax1.scatter(Xc[0, t, 0], Xc[1, t, 0], Xc[2, t, 0], c='g', s=2*t+5)
-    ax1.scatter(Xc[0, t, 1], Xc[1, t, 1], Xc[2, t, 1], c='y', s=2*t+5) 
+    ax1.scatter(Xc[0, t, 0], Xc[1, t, 0], Xc[2, t, 0], c='g', s=3*t+5)
+    ax1.scatter(Xc[0, t, 1], Xc[1, t, 1], Xc[2, t, 1], c='y', s=3*t+5) 
     ax1.scatter(X_proj['t'][0, t, 0], X_proj['t'][1, t, 0], X_proj['t'][2, t, 0], 
-                c='g', s=2*t+5, marker='x')
+                c='g', s=3*t+5, marker='x')
     ax1.scatter(X_proj['t'][0, t, 1], X_proj['t'][1, t, 1], X_proj['t'][2, t, 1], 
-                c='y', s=2*t+5, marker='x')
+                c='y', s=3*t+5, marker='x')
     
     ax1.scatter(X_proj['st'][0, t, 0], X_proj['st'][1, t, 0], X_proj['st'][2, t, 0], 
-                c='g', s=2*t+5, marker='v')
+                c='g', s=3*t+5, marker='v')
     ax1.scatter(X_proj['st'][0, t, 1], X_proj['st'][1, t, 1], X_proj['st'][2, t, 1], 
-                c='y', s=2*t+5, marker='v')
+                c='y', s=3*t+5, marker='v')
     
     ax1.scatter(X_proj['s'][0, t, 0], X_proj['s'][1, t, 0], X_proj['s'][2, t, 0], 
-                c='g', s=2*t+5, marker='+')
+                c='g', s=3*t+5, marker='+')
     ax1.scatter(X_proj['s'][0, t, 1], X_proj['s'][1, t, 1], X_proj['s'][2, t, 1], 
-                c='y', s=2*t+5, marker='+')
+                c='y', s=3*t+5, marker='+')
 ax1.set_xlabel('Neuron1')
 ax1.set_ylabel('Neuron2')
 ax1.set_zlabel('Neuron3')
@@ -100,6 +101,60 @@ plt.show()
 
 
 
+time = np.arange(T)
+
+plt.figure(figsize=(16, 7))
+plt.subplot(131)
+
+for s in range(S):
+    plt.plot(time, X_proj['t'][0, :, s])
+
+plt.title('1st time component')
+    
+plt.subplot(132)
+
+for s in range(S):
+    plt.plot(time, X_proj['s'][0, :, s])
+    
+plt.title('1st stimulus component')
+    
+plt.subplot(133)
+
+for s in range(S):
+    plt.plot(time, X_proj['st'][0, :, s])
+    
+plt.title('1st mixing component')
+plt.show()
+
+## Same but for the data non centered
+## Fit and transform the data to the new axes
+#X_proj = dpca.fit_transform(X, Xtrial)
+#
+## ploting the new projection agains the old 
+#fig = plt.figure()
+#ax1 = fig.add_subplot(111, projection='3d')
+#for t in range(T):
+#    ax1.scatter(X[0, t, 0], X[1, t, 0], X[2, t, 0], c='g', s=3*t+5)
+#    ax1.scatter(X[0, t, 1], X[1, t, 1], X[2, t, 1], c='y', s=3*t+5) 
+#    ax1.scatter(X_proj['t'][0, t, 0], X_proj['t'][1, t, 0], X_proj['t'][2, t, 0], 
+#                c='g', s=3*t+5, marker='x')
+#    ax1.scatter(X_proj['t'][0, t, 1], X_proj['t'][1, t, 1], X_proj['t'][2, t, 1], 
+#                c='y', s=3*t+5, marker='x')
+#    
+#    ax1.scatter(X_proj['ts'][0, t, 0], X_proj['ts'][1, t, 0], X_proj['ts'][2, t, 0], 
+#                c='g', s=3*t+5, marker='v')
+#    ax1.scatter(X_proj['ts'][0, t, 1], X_proj['ts'][1, t, 1], X_proj['ts'][2, t, 1], 
+#                c='y', s=3*t+5, marker='v')
+#    
+#    ax1.scatter(X_proj['s'][0, t, 0], X_proj['s'][1, t, 0], X_proj['s'][2, t, 0], 
+#                c='g', s=3*t+5, marker='+')
+#    ax1.scatter(X_proj['s'][0, t, 1], X_proj['s'][1, t, 1], X_proj['s'][2, t, 1], 
+#                c='y', s=3*t+5, marker='+')
+#ax1.set_xlabel('Neuron1')
+#ax1.set_ylabel('Neuron2')
+#ax1.set_zlabel('Neuron3')
+#
+#plt.show()
 
 
     

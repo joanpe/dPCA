Some bug with loss calculation

python/dPCA/dualtask/DualTask.py
@@ -180,11 +180,11 @@ class DualTask(RecurrentWhisperer):
                 tf.squared_difference(self.output_bxtxd[:, n_time-1, :],
                                       self.pred_output_bxtxd[:, n_time-1, :]))
         else:
-            self.loss = tf.reduce_mean(
-                    tf.squared_difference(self.output_bxtxd[:, np.logical_or(
-                            gng_time, n_time-1), :],
-                            self.pred_output_bxtxd[:, np.logical_or(
-                                    gng_time, n_time-1), :]))
+            w = np.where((np.arange(n_time) == gng_time) +
+                         (np.arange(n_time) == n_time-1))[0]
+            self.loss = tf.reduce_mean(tf.squared_difference(
+                    self.output_bxtxd[:, w.astype(np.int32), :],
+                    self.pred_output_bxtxd[:, w.astype(np.int32), :]))
 
     def _setup_saver(self):
         '''See docstring in RecurrentWhisperer.'''

python/dPCA/dualtask/run_DualTask.py
@@ -76,16 +76,19 @@ for inst in range(5):
         summary = dt._train_batch(example_trials)
         loss = summary['loss']
 
-        data = {'inst': inst, 'noise': noise, 'loss': loss}
         data_mat[inst, np.where(noise_rng == noise)[0]] = loss
+
+        data = {'inst': inst, 'noise': noise, 'loss': loss}
 
         fig_dir = os.path.join(PATH, 'data')
         try:
             os.mkdir(fig_dir)
         except OSError:
-            np.savez(os.path.join(fig_dir, 'noise_'+str(noise)), **data)
+            np.savez(os.path.join(fig_dir, 'noise_' + str(noise) +
+                                  '_inst_' + str(inst)), **data)
         else:
-            np.savez(os.path.join(fig_dir, 'noise_'+str(noise)), **data)
+            np.savez(os.path.join(fig_dir, 'noise_' + str(noise) +
+                                  '_inst_' + str(inst)), **data)
 
 # f.canvas.draw()
 # *****************************************************************************
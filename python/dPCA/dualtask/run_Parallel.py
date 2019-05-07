1c2dbc9b012210aa4dc019a5fc60b876b64c590e
joanpe <joanperramonllussa@gmail.com>
2019-05-07 16:56:41 +0200

    Solved bug for parallel processing

--- python/dPCA/dualtask/run_Parallel.py
+++ python/dPCA/dualtask/run_Parallel.py
@@ -30,6 +30,7 @@ import multiprocessing
 noise_rng = np.array([0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6])
 # Time of appearence of the go- no go task. 0 for no task.
 gng_rng = np.array([0, 10])
+lamb = 0, delay_max = 0
 
 
 def trainDualTask(noise, gng, inst):
@@ -47,15 +48,15 @@ def trainDualTask(noise, gng, inst):
         'min_learning_rate': 1e-5,
         'max_n_epochs': 5000,
         'do_restart_run': True,
-        'log_dir': './logs/',
+        'log_dir': './logs_' + str(gng) + '/' + str(inst),
         'data_hps': {
             'n_batch': 2048,
             'n_time': 20,
             'n_bits': 6,
             'noise': noise,
             'gng_time': gng,
-            'lamb': 0,
-            'delay_max': 0},
+            'lamb': lamb,
+            'delay_max': delay_max},
         'alr_hps': alr_hps
         }
 
@@ -104,10 +105,10 @@ for gng in gng_rng:
             acc_dpa.append(acc[i][0])
             acc_gng.append(acc[i][1])
         # Save data in a list
-        datalist.append([noise, acc_dpa, acc_gng])
+        NOISE = np.repeat(noise, INST)
+        datalist.append([NOISE, acc_dpa, acc_gng])
         # Plot loss / accuracy for the different noise- instances
         plt.figure(f.number)
-        NOISE = np.repeat(noise, INST)
 #        plt.plot(noise, loss_dpa, '+')
 #        plt.plot(noise, loss_gng, 'v')
         plt.plot(NOISE, acc_dpa, '+', color='k')
@@ -121,26 +122,64 @@ for gng in gng_rng:
 
     # save data and figure
     data = {'datalist': datalist}
-#
-#    fig_dir = os.path.join(PATH, 'data_trainedwithnoise')
-#    try:
-#        os.mkdir(fig_dir)
-#    except OSError:
-#        np.savez(os.path.join(fig_dir, 'data_' + str(gng_time) + '_'
-#                              + str(lamb) + '_' + str(delay_max)
-#                              + '_i' + str(INST) + '_n' + str(noise_rng[0])
-#                              + '-' + str(noise_rng[-1])), **data)
-#        plt.savefig(os.path.join(fig_dir, 'acc_inst_' + str(gng_time) + '_'
-#                                 + str(lamb) + '_' + str(delay_max)
-#                                 + '_i' + str(INST) + '_n' + str(noise_rng[0])
-#                                 + '-' + str(noise_rng[-1]) + '.png'))
-#    else:
-#        np.savez(os.path.join(fig_dir, 'data_' + str(gng_time) + '_'
-#                              + str(lamb) + '_' + str(delay_max)
-#                              + '_i' + str(INST) + '_n' + str(noise_rng[0])
-#                              + '-' + str(noise_rng[-1])), **data)
-#        plt.savefig(os.path.join(fig_dir, 'acc_inst_' + str(gng_time) + '_'
-#                                 + str(lamb) + '_' + str(delay_max)
-#                                 + '_i' + str(INST) + '_n' + str(noise_rng[0])
-#                                 + '-' + str(noise_rng[-1]) + '.png'))
 
+    fig_dir = os.path.join(PATH, 'data_trainedwithnoise')
+    try:
+        os.mkdir(fig_dir)
+    except OSError:
+        np.savez(os.path.join(fig_dir, 'data_' + str(gng) + '_'
+                              + str(lamb) + '_' + str(delay_max)
+                              + '_i' + str(INST) + '_n' + str(noise_rng[0])
+                              + '-' + str(noise_rng[-1])), **data)
+        plt.savefig(os.path.join(fig_dir, 'acc_noise_' + str(gng) + '_'
+                                 + str(lamb) + '_' + str(delay_max)
+                                 + '_i' + str(INST) + '_n' + str(noise_rng[0])
+                                 + '-' + str(noise_rng[-1]) + '.png'))
+    else:
+        np.savez(os.path.join(fig_dir, 'data_' + str(gng) + '_'
+                              + str(lamb) + '_' + str(delay_max)
+                              + '_i' + str(INST) + '_n' + str(noise_rng[0])
+                              + '-' + str(noise_rng[-1])), **data)
+        plt.savefig(os.path.join(fig_dir, 'acc_noise_' + str(gng) + '_'
+                                 + str(lamb) + '_' + str(delay_max)
+                                 + '_i' + str(INST) + '_n' + str(noise_rng[0])
+                                 + '-' + str(noise_rng[-1]) + '.png'))
+
+'''Plot data together'''
+# Loading the data for task without distractor
+data = np.load(fig_dir + '/data_0_0_0' + '_i' + str(INST) + '_n' +
+               str(noise_rng[0]) + '-' + str(noise_rng[-1]) + '.npz')
+data = np.hstack(data['datalist'][i] for i in range(noise_rng.shape[0]))
+
+# Compute the mean accuracy across instances
+mean_acc = []
+std = []
+for n in noise_rng:
+    mean_acc.append(np.mean(data[1, data[0, :] == n]))
+    std.append(np.std(data[1, data[0, :] == n]))
+# Plot with error bars of the accuracy / loss
+plt.figure()
+plt.errorbar(noise_rng, mean_acc, yerr=std, marker='+',
+             label='DPA accuracy dpa gng0')
+
+# Loading data for task with distractor
+data = np.load(fig_dir + '/data_10_0_0' + '_i' + str(INST) + '_n' +
+               str(noise_rng[0]) + '-' + str(noise_rng[-1]) + '.npz')
+data = np.hstack(data['datalist'][i] for i in range(noise_rng.shape[0]))
+
+# Compute the mean accuracy across instances
+mean_acc = []
+std = []
+for n in noise_rng:
+    mean_acc.append(np.mean(data[1, data[0, :] == n]))
+    std.append(np.std(data[1, data[0, :] == n]))
+
+# Plot with error bars of the accuracy / loss
+plt.errorbar(noise_rng, mean_acc, yerr=std, marker='+',
+             label='DPA accuracy gng10')
+
+
+plt.xlabel('Noise')
+plt.ylabel('Mean accuracy')
+plt.legend()
+plt.show()
